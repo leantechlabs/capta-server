42634e85dfcf4156ef6bf5459172092684fbd44e
leantechlabs <info.leantechlabs@gmail.com>
2023-10-19 13:55:31 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-import express from "express"
+import dotenv from 'dotenv'
+dotenv.config(); 
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-const app = express()
-const port = 3000
-app.use("/",(req,res)=>{
-  res.json({message:"Server is Running "});
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB connection
+// mongoose.connect('mongodb://localhost:27017/captaDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// const connection = mongoose.connection;
+
+// connection.once('open', () => {
+//     console.log('MongoDB database connection established successfully');
+// });
+
+// Example API routes
+//const userRouter = require('./routes/user');
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
-app.listen(3000,()=>{
-  console.log(`server running on https://capta-server.vercel.app/:${port}`)
-})
