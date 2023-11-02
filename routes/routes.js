1081eca24d1652b51b31f6fcf550c40e6249509f
GANESH985 <nayakg985@gmail.com>
2023-11-02 23:30:00 +0530

    mou create and confirmation

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,15 @@
+import express from require('express');
+
+const {mouCreate}= require('.')
+
+const router = express.Router();
+
+router.post('/mou/create',mouCreate);
+router.post('/mou/confirm', mouConf);
 // app.use('/api/user', userRouter);
 // app.use('/api/college', collegeRouter);
 // app.use('/api/mou', mouRouter);
 // // Add more routes...
+
+
+module.exports = router;
